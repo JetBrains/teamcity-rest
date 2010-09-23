8720cde7fc33e5be92276b1d09605624fc37efc2
Yegor.Yarko <Yegor.Yarko@jetbrains.com>
2010-09-23 12:37:54 +0000

    Response with plain text error in case of RuntimeException.

---
+++ src/jetbrains/buildServer/server/rest/errors/RuntimeExceptionMapper.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2000-2010 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package jetbrains.buildServer.server.rest.errors;
+
+import com.intellij.openapi.diagnostic.Logger;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+import jetbrains.buildServer.server.rest.jersey.ExceptionMapperUtil;
+
+/**
+ * User: Yegor Yarko
+ * Date: 30.03.2009
+ */
+@Provider
+public class RuntimeExceptionMapper extends ExceptionMapperUtil implements ExceptionMapper<RuntimeException> {
+  protected static final Logger LOG = Logger.getInstance(RuntimeExceptionMapper.class.getName());
+
+  public Response toResponse(RuntimeException exception) {
+    return reportError(Response.Status.INTERNAL_SERVER_ERROR, exception, "Error occurred while processing this request.");
+  }
+}
