001212066d42cd1bfb87ec713214c627ccc08720
Vladislav.Rassokhin <Vladislav.Rassokhin@jetbrains.com>
2013-02-26 15:01:17 +0000

    REST API: ObjectMapperResolver renamed.

--- src/jetbrains/buildServer/server/rest/jersey/JacksonObjectMapperResolver.java
+++ src/jetbrains/buildServer/server/rest/jersey/JacksonObjectMapperResolver.java
@@ -1,11 +1,6 @@
 package jetbrains.buildServer.server.rest.jersey;
 
 import com.intellij.openapi.diagnostic.Logger;
-import java.text.SimpleDateFormat;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.ext.ContextResolver;
-import javax.ws.rs.ext.Provider;
 import jetbrains.buildServer.server.rest.APIController;
 import jetbrains.buildServer.server.rest.model.Constants;
 import jetbrains.buildServer.serverSide.TeamCityProperties;
@@ -14,18 +9,24 @@ import org.codehaus.jackson.map.SerializationConfig;
 import org.codehaus.jackson.map.annotate.JsonSerialize;
 import org.codehaus.jackson.xc.JaxbAnnotationIntrospector;
 
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.ext.ContextResolver;
+import javax.ws.rs.ext.Provider;
+import java.text.SimpleDateFormat;
+
 /**
  * @author Vladislav.Rassokhin
  */
 @Provider
 @Produces(MediaType.APPLICATION_JSON)
-public class ObjectMapperResolver implements ContextResolver<ObjectMapper> {
+public class JacksonObjectMapperResolver implements ContextResolver<ObjectMapper> {
 
-  final static Logger LOG = Logger.getInstance(ObjectMapperResolver.class.getName());
+  final static Logger LOG = Logger.getInstance(JacksonObjectMapperResolver.class.getName());
 
   private final ObjectMapper myMapper;
 
-  public ObjectMapperResolver() {
+  public JacksonObjectMapperResolver() {
     myMapper = new ObjectMapper();
     myMapper.setSerializationInclusion(JsonSerialize.Inclusion.NON_NULL);
     myMapper.setAnnotationIntrospector(new JaxbAnnotationIntrospector());
