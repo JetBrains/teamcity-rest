498414d1c7739a424b12a88256369ff3652ab772
Yegor.Yarko <Yegor.Yarko@jetbrains.com>
2013-06-01 16:29:03 +0000

    REST: support getting/setting branches in VCS labeling (TW-24983)

--- src/jetbrains/buildServer/server/rest/model/buildType/VCSLabelingOptions.java
+++ src/jetbrains/buildServer/server/rest/model/buildType/VCSLabelingOptions.java
@@ -2,6 +2,7 @@ package jetbrains.buildServer.server.rest.model.buildType;
 
 import java.util.Arrays;
 import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import jetbrains.buildServer.server.rest.ApiUrlBuilder;
 import jetbrains.buildServer.server.rest.errors.BadRequestException;
@@ -23,12 +24,16 @@ public class VCSLabelingOptions {
   @XmlAttribute(name = "type")
   public String type;
 
+  @XmlElement(name = "branchFilter")
+  public String branchFilter;
+
   public VCSLabelingOptions() {
   }
 
   public VCSLabelingOptions(@NotNull final BuildTypeOrTemplate buildType, @NotNull final ApiUrlBuilder apiUrlBuilder) {
     labelName = buildType.get().getLabelPattern();
     type = buildType.get().getLabelingType().toString();
+    branchFilter = buildType.get().getBranchFilter();
   }
 
   public void applyTo(final BuildTypeOrTemplate buildType, @NotNull final BeanContext context) {
@@ -48,5 +53,10 @@ public class VCSLabelingOptions {
 
     buildType.get().setLabelPattern(labelName);
     buildType.get().setLabelingType(labelingType);
+    if (branchFilter != null){
+      buildType.get().setBranchFilter(branchFilter);
+    }else{
+      buildType.get().setBranchFilter(""); //todo: TeamCity API: not clear if this is correct way to reset the value
+    }
   }
 }
