3229df0cd17e5461afd2573f064a488dc5be1c81
Yegor.Yarko <Yegor.Yarko@jetbrains.com>
2018-02-28 09:31:58 +0000

    REST: minor logging improvement

--- src/jetbrains/buildServer/server/rest/data/FinderImpl.java
+++ src/jetbrains/buildServer/server/rest/data/FinderImpl.java
@@ -344,8 +344,8 @@ public class FinderImpl<ITEM> implements Finder<ITEM> {
     if (totalItemsProcessed >= TeamCityProperties.getLong("rest.finder.processedItemsLogLimit", 1)) {
       final String lookupLimitMessage =
         filter.isLookupLimitReached() ? " (lookupLimit of " + filter.getLookupLimit() + " reached). Last processed item: " + LogUtil.describe(filter.getLastProcessedItem()) : "";
-      LOG.debug("While processing locator '" + locator + "', " + result.size() + " items were matched by the filter from " + totalItemsProcessed + " processed in total" +
-                lookupLimitMessage + ", took " + processingTimeMs + " ms"); //todo make AbstractFilter loggable and add logging here
+      LOG.debug("While processing locator '" + locator + "' by finder " + getClass().getName() + ", " + result.size() + " items were matched by the filter from " + totalItemsProcessed + " processed in total" +
+                lookupLimitMessage + ", took " + processingTimeMs + " ms"); //todo make AbstractFilter loggable and add logging here instead of getClass().getName()
     }
     if (totalItemsProcessed > TeamCityProperties.getLong("rest.finder.processedItemsWarnLimit", 10000) ||
         processingTimeMs > TeamCityProperties.getLong("rest.finder.timeWarnLimit", 10000)) {
