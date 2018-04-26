066cb78252db20f4106c2702175afd5aee703e43
Andrey.Titov <Andrey.Titov@jetbrains.com>
2018-04-26 10:33:45 +0000

    Fix test in rest: ignore order

--- test/jetbrains/buildServer/server/rest/data/PermissionAssignmentFinderTest.java
+++ test/jetbrains/buildServer/server/rest/data/PermissionAssignmentFinderTest.java
@@ -18,13 +18,18 @@ package jetbrains.buildServer.server.rest.data;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Supplier;
 import jetbrains.buildServer.server.rest.errors.BadRequestException;
 import jetbrains.buildServer.server.rest.errors.LocatorProcessException;
+import jetbrains.buildServer.server.rest.errors.NotFoundException;
 import jetbrains.buildServer.serverSide.auth.Permission;
 import jetbrains.buildServer.serverSide.auth.Role;
 import jetbrains.buildServer.serverSide.auth.RoleScope;
 import jetbrains.buildServer.serverSide.impl.ProjectEx;
 import jetbrains.buildServer.users.SUser;
+import org.assertj.core.api.BDDAssertions;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -52,38 +57,43 @@ public class PermissionAssignmentFinderTest extends BaseFinderTest<PermissionAss
     myProject11 = myProject1.createProject("project11", "project11");
   }
 
+  private void checkUnordered(@Nullable final String locator, @NotNull final PermissionAssignmentData... items) {
+    check(locator, BaseFinderTest::getDescription, BaseFinderTest::getDescription, getFinder(), new UnorderedMatchStrategy(), items);
+  }
+
   @Test
   public void testProjectPermissions() throws Exception {
     Role roleProject = getTestRoles().createRole(Permission.VIEW_PROJECT, Permission.RUN_BUILD);
     String projectId = myProject1.getProjectId();
     myUser1.addRole(RoleScope.projectScope(projectId), roleProject);
 
-    check(null, pa(Permission.CHANGE_OWN_PROFILE), pa(Permission.VIEW_PROJECT, getRootProject().getProjectId()), pa(Permission.RUN_BUILD, projectId), pa(Permission.VIEW_PROJECT, projectId), pa(Permission.RUN_BUILD, myProject11.getProjectId()), pa(Permission.VIEW_PROJECT, myProject11.getProjectId()));
-    check("global:any", pa(Permission.CHANGE_OWN_PROFILE), pa(Permission.VIEW_PROJECT, getRootProject().getProjectId()), pa(Permission.RUN_BUILD, projectId), pa(Permission.VIEW_PROJECT, projectId), pa(Permission.RUN_BUILD, myProject11.getProjectId()), pa(Permission.VIEW_PROJECT, myProject11.getProjectId()));
-    check("global:true", pa(Permission.CHANGE_OWN_PROFILE));
-    check("global:false", pa(Permission.VIEW_PROJECT, getRootProject().getProjectId()), pa(Permission.RUN_BUILD, projectId), pa(Permission.VIEW_PROJECT, projectId), pa(Permission.RUN_BUILD, myProject11.getProjectId()), pa(Permission.VIEW_PROJECT, myProject11.getProjectId()));
-    check("project:(id:" + myProject1.getExternalId() + ")", pa(Permission.RUN_BUILD, projectId), pa(Permission.VIEW_PROJECT, projectId));
-    check("project:(id:" + getRootProject().getExternalId() + ")", pa(Permission.VIEW_PROJECT, getRootProject().getProjectId()));
-    check("permission:" + "RUN_BUILD", pa(Permission.RUN_BUILD, projectId), pa(Permission.RUN_BUILD, myProject11.getProjectId()));
-    check("permission:" + "run_build" + ",global:true");
-    check("permission:" + "view_PrOjEcT", pa(Permission.VIEW_PROJECT, getRootProject().getProjectId()), pa(Permission.VIEW_PROJECT, projectId), pa(Permission.VIEW_PROJECT, myProject11.getProjectId()));
-    check("permission:" + "view_project" + ",project:" + projectId, pa(Permission.VIEW_PROJECT, projectId));
+    checkUnordered(null, pa(Permission.CHANGE_OWN_PROFILE), pa(Permission.VIEW_PROJECT, getRootProject().getProjectId()), pa(Permission.RUN_BUILD, projectId), pa(Permission.VIEW_PROJECT, projectId),
+                   pa(Permission.RUN_BUILD, myProject11.getProjectId()), pa(Permission.VIEW_PROJECT, myProject11.getProjectId()));
+    checkUnordered("global:any", pa(Permission.CHANGE_OWN_PROFILE), pa(Permission.VIEW_PROJECT, getRootProject().getProjectId()), pa(Permission.RUN_BUILD, projectId), pa(Permission.VIEW_PROJECT, projectId), pa(Permission.RUN_BUILD, myProject11.getProjectId()), pa(Permission.VIEW_PROJECT, myProject11.getProjectId()));
+    checkUnordered("global:true", pa(Permission.CHANGE_OWN_PROFILE));
+    checkUnordered("global:false", pa(Permission.VIEW_PROJECT, getRootProject().getProjectId()), pa(Permission.RUN_BUILD, projectId), pa(Permission.VIEW_PROJECT, projectId), pa(Permission.RUN_BUILD, myProject11.getProjectId()), pa(Permission.VIEW_PROJECT, myProject11.getProjectId()));
+    checkUnordered("project:(id:" + myProject1.getExternalId() + ")", pa(Permission.RUN_BUILD, projectId), pa(Permission.VIEW_PROJECT, projectId));
+    checkUnordered("project:(id:" + getRootProject().getExternalId() + ")", pa(Permission.VIEW_PROJECT, getRootProject().getProjectId()));
+    checkUnordered("permission:" + "RUN_BUILD", pa(Permission.RUN_BUILD, projectId), pa(Permission.RUN_BUILD, myProject11.getProjectId()));
+    checkUnordered("permission:" + "run_build" + ",global:true");
+    checkUnordered("permission:" + "view_PrOjEcT", pa(Permission.VIEW_PROJECT, getRootProject().getProjectId()), pa(Permission.VIEW_PROJECT, projectId), pa(Permission.VIEW_PROJECT, myProject11.getProjectId()));
+    checkUnordered("permission:" + "view_project" + ",project:" + projectId, pa(Permission.VIEW_PROJECT, projectId));
 
     ProjectEx project2 = createProject("prj2", "prj2");
     Role role2 = getTestRoles().createRole(Permission.RUN_BUILD);
     myUser1.addRole(RoleScope.projectScope(project2.getProjectId()), role2);
 
-    check("permission:run_build,project:(item:(id:" + myProject1.getExternalId() + "),item:(id:" + project2.getExternalId() + "))", pa(Permission.RUN_BUILD, projectId),
-          pa(Permission.RUN_BUILD, project2.getProjectId()));
+    checkUnordered("permission:run_build,project:(item:(id:" + myProject1.getExternalId() + "),item:(id:" + project2.getExternalId() + "))", pa(Permission.RUN_BUILD, projectId),
+                   pa(Permission.RUN_BUILD, project2.getProjectId()));
 
-    check("item:(permission:view_project,project:(id:" + myProject1.getExternalId() + ")),item:(permission:view_project,project:(id:" + project2.getExternalId() + "))",
-          pa(Permission.VIEW_PROJECT, projectId));
+    checkUnordered("item:(permission:view_project,project:(id:" + myProject1.getExternalId() + ")),item:(permission:view_project,project:(id:" + project2.getExternalId() + "))",
+                   pa(Permission.VIEW_PROJECT, projectId));
 
-    check("item:(permission:view_project,project:(id:" + project2.getExternalId() + ")),item:(permission:run_build,project:(id:" + project2.getExternalId() + "))",
-          pa(Permission.RUN_BUILD, project2.getProjectId()));
+    checkUnordered("item:(permission:view_project,project:(id:" + project2.getExternalId() + ")),item:(permission:run_build,project:(id:" + project2.getExternalId() + "))",
+                   pa(Permission.RUN_BUILD, project2.getProjectId()));
 
-    check("item:(permission:CHANGE_OWN_PROFILE),item:(permission:run_build,project:(id:" + myProject1.getExternalId() + "))",
-          pa(Permission.CHANGE_OWN_PROFILE), pa(Permission.RUN_BUILD, projectId));
+    checkUnordered("item:(permission:CHANGE_OWN_PROFILE),item:(permission:run_build,project:(id:" + myProject1.getExternalId() + "))",
+                   pa(Permission.CHANGE_OWN_PROFILE), pa(Permission.RUN_BUILD, projectId));
 
     checkExceptionOnItemsSearch(BadRequestException.class, "permission:XXX");
     checkExceptionOnItemsSearch(LocatorProcessException.class, "project:(id:XXX)");
@@ -95,13 +105,13 @@ public class PermissionAssignmentFinderTest extends BaseFinderTest<PermissionAss
     String projectId = myProject1.getProjectId();
     myUser1.addRole(RoleScope.globalScope(), roleProject);
 
-    check(null, pa(Permission.RUN_BUILD), pa(Permission.VIEW_PROJECT), pa(Permission.CHANGE_OWN_PROFILE), pa(Permission.ADMINISTER_AGENT));
-    check("global:true", pa(Permission.RUN_BUILD), pa(Permission.VIEW_PROJECT), pa(Permission.CHANGE_OWN_PROFILE), pa(Permission.ADMINISTER_AGENT));
-    check("global:false");
-    check("project:(internalId:" + projectId + ")", pa(Permission.RUN_BUILD), pa(Permission.VIEW_PROJECT));
-    check("project:(internalId:" + getRootProject().getProjectId() + ")", pa(Permission.RUN_BUILD), pa(Permission.VIEW_PROJECT));
-    check("permission:" + "run_build", pa(Permission.RUN_BUILD));
-    check("permission:" + "run_build" + ",project:" + projectId, pa(Permission.RUN_BUILD));
+    checkUnordered(null, pa(Permission.RUN_BUILD), pa(Permission.VIEW_PROJECT), pa(Permission.CHANGE_OWN_PROFILE), pa(Permission.ADMINISTER_AGENT));
+    checkUnordered("global:true", pa(Permission.RUN_BUILD), pa(Permission.VIEW_PROJECT), pa(Permission.CHANGE_OWN_PROFILE), pa(Permission.ADMINISTER_AGENT));
+    checkUnordered("global:false");
+    checkUnordered("project:(internalId:" + projectId + ")", pa(Permission.RUN_BUILD), pa(Permission.VIEW_PROJECT));
+    checkUnordered("project:(internalId:" + getRootProject().getProjectId() + ")", pa(Permission.RUN_BUILD), pa(Permission.VIEW_PROJECT));
+    checkUnordered("permission:" + "run_build", pa(Permission.RUN_BUILD));
+    checkUnordered("permission:" + "run_build" + ",project:" + projectId, pa(Permission.RUN_BUILD));
   }
 
   @Test
@@ -111,13 +121,13 @@ public class PermissionAssignmentFinderTest extends BaseFinderTest<PermissionAss
     myUser1.addRole(RoleScope.projectScope(getRootProject().getProjectId()), roleProject);
     //TeamCity API issue: this has the same effect as globally assigned role, but permissions are per-project in the case. Thus results differ (e.g. see "global:true")
 
-    check(null, pa(Permission.CHANGE_OWN_PROFILE), pa(Permission.ADMINISTER_AGENT), pa(Permission.RUN_BUILD, getRootProject().getProjectId()), pa(Permission.VIEW_PROJECT, getRootProject().getProjectId()), pa(Permission.RUN_BUILD, projectId), pa(Permission.VIEW_PROJECT, projectId), pa(Permission.RUN_BUILD, myProject11.getProjectId()), pa(Permission.VIEW_PROJECT, myProject11.getProjectId()));
-    check("global:true", pa(Permission.CHANGE_OWN_PROFILE), pa(Permission.ADMINISTER_AGENT));
-    check("global:false", pa(Permission.RUN_BUILD, getRootProject().getProjectId()), pa(Permission.VIEW_PROJECT, getRootProject().getProjectId()), pa(Permission.RUN_BUILD, projectId), pa(Permission.VIEW_PROJECT, projectId), pa(Permission.RUN_BUILD, myProject11.getProjectId()), pa(Permission.VIEW_PROJECT, myProject11.getProjectId()));
-    check("project:(internalId:" + projectId + ")", pa(Permission.RUN_BUILD, projectId), pa(Permission.VIEW_PROJECT, projectId));
-    check("project:(internalId:" + getRootProject().getProjectId() + ")", pa(Permission.RUN_BUILD, getRootProject().getProjectId()), pa(Permission.VIEW_PROJECT, getRootProject().getProjectId()));
-    check("permission:" + "run_build", pa(Permission.RUN_BUILD, getRootProject().getProjectId()), pa(Permission.RUN_BUILD, projectId), pa(Permission.RUN_BUILD, myProject11.getProjectId()));
-    check("permission:" + "run_build" + ",project:" + projectId, pa(Permission.RUN_BUILD, projectId));
+    checkUnordered(null, pa(Permission.CHANGE_OWN_PROFILE), pa(Permission.ADMINISTER_AGENT), pa(Permission.RUN_BUILD, getRootProject().getProjectId()), pa(Permission.VIEW_PROJECT, getRootProject().getProjectId()), pa(Permission.RUN_BUILD, projectId), pa(Permission.VIEW_PROJECT, projectId), pa(Permission.RUN_BUILD, myProject11.getProjectId()), pa(Permission.VIEW_PROJECT, myProject11.getProjectId()));
+    checkUnordered("global:true", pa(Permission.CHANGE_OWN_PROFILE), pa(Permission.ADMINISTER_AGENT));
+    checkUnordered("global:false", pa(Permission.RUN_BUILD, getRootProject().getProjectId()), pa(Permission.VIEW_PROJECT, getRootProject().getProjectId()), pa(Permission.RUN_BUILD, projectId), pa(Permission.VIEW_PROJECT, projectId), pa(Permission.RUN_BUILD, myProject11.getProjectId()), pa(Permission.VIEW_PROJECT, myProject11.getProjectId()));
+    checkUnordered("project:(internalId:" + projectId + ")", pa(Permission.RUN_BUILD, projectId), pa(Permission.VIEW_PROJECT, projectId));
+    checkUnordered("project:(internalId:" + getRootProject().getProjectId() + ")", pa(Permission.RUN_BUILD, getRootProject().getProjectId()), pa(Permission.VIEW_PROJECT, getRootProject().getProjectId()));
+    checkUnordered("permission:" + "run_build", pa(Permission.RUN_BUILD, getRootProject().getProjectId()), pa(Permission.RUN_BUILD, projectId), pa(Permission.RUN_BUILD, myProject11.getProjectId()));
+    checkUnordered("permission:" + "run_build" + ",project:" + projectId, pa(Permission.RUN_BUILD, projectId));
   }
 
   @Test
@@ -127,13 +137,13 @@ public class PermissionAssignmentFinderTest extends BaseFinderTest<PermissionAss
 
     myUser1.addRole(RoleScope.projectScope(projectId), roleProject);
 
-    check(null, pa(Permission.CHANGE_OWN_PROFILE), pa(Permission.ADMINISTER_AGENT), pa(Permission.RUN_BUILD, projectId), pa(Permission.RUN_BUILD, myProject11.getProjectId()));
-    check("global:true", pa(Permission.CHANGE_OWN_PROFILE), pa(Permission.ADMINISTER_AGENT));
-    check("global:false", pa(Permission.RUN_BUILD, projectId), pa(Permission.RUN_BUILD, myProject11.getProjectId()));
-    check("project:(internalId:" + projectId + ")", pa(Permission.RUN_BUILD, projectId));
-    check("project:(internalId:" + getRootProject().getProjectId() + ")");
-    check("permission:" + "run_build", pa(Permission.RUN_BUILD, projectId), pa(Permission.RUN_BUILD, myProject11.getProjectId()));
-    check("permission:" + "run_build" + ",project:" + projectId, pa(Permission.RUN_BUILD, projectId));
+    checkUnordered(null, pa(Permission.CHANGE_OWN_PROFILE), pa(Permission.ADMINISTER_AGENT), pa(Permission.RUN_BUILD, projectId), pa(Permission.RUN_BUILD, myProject11.getProjectId()));
+    checkUnordered("global:true", pa(Permission.CHANGE_OWN_PROFILE), pa(Permission.ADMINISTER_AGENT));
+    checkUnordered("global:false", pa(Permission.RUN_BUILD, projectId), pa(Permission.RUN_BUILD, myProject11.getProjectId()));
+    checkUnordered("project:(internalId:" + projectId + ")", pa(Permission.RUN_BUILD, projectId));
+    checkUnordered("project:(internalId:" + getRootProject().getProjectId() + ")");
+    checkUnordered("permission:" + "run_build", pa(Permission.RUN_BUILD, projectId), pa(Permission.RUN_BUILD, myProject11.getProjectId()));
+    checkUnordered("permission:" + "run_build" + ",project:" + projectId, pa(Permission.RUN_BUILD, projectId));
   }
 
   @Test
@@ -148,13 +158,13 @@ public class PermissionAssignmentFinderTest extends BaseFinderTest<PermissionAss
     List<PermissionAssignmentData> global = getFinder().getItems("global:true").myEntries;
     assertEquals(all.size(), global.size());
 
-    check("global:false");
+    checkUnordered("global:false");
 
     List<PermissionAssignmentData> project1Permissions = getFinder().getItems("project:" + projectId).myEntries;
     assertEquals(Arrays.stream(Permission.values()).filter(p -> p.isProjectAssociationSupported()).count(), project1Permissions.size());
 
-    check("permission:" + "run_build", pa(Permission.RUN_BUILD));
-    check("permission:" + "run_build" + ",project:" + projectId, pa(Permission.RUN_BUILD));
+    checkUnordered("permission:" + "run_build", pa(Permission.RUN_BUILD));
+    checkUnordered("permission:" + "run_build" + ",project:" + projectId, pa(Permission.RUN_BUILD));
   }
 
 
@@ -164,5 +174,21 @@ public class PermissionAssignmentFinderTest extends BaseFinderTest<PermissionAss
 
   private PermissionAssignmentData pa(final Permission permission) {
     return new PermissionAssignmentData(permission);
+  }
+
+  private static class UnorderedMatchStrategy implements CollectionsMatchStrategy<PermissionAssignmentData, PermissionAssignmentData> {
+    @Override
+    public void matchCollection(@NotNull final PermissionAssignmentData[] items, @NotNull final List<PermissionAssignmentData> result) {
+      BDDAssertions.then(items).containsAll(result);
+    }
+
+    @Override
+    public void matchSingle(final PermissionAssignmentData[] items, final Supplier<PermissionAssignmentData> singleResultSupplier) {
+      if (items.length == 0) {
+        assertExceptionThrown(singleResultSupplier::get, NotFoundException.class);
+      } else {
+        BDDAssertions.then(items).contains(singleResultSupplier.get());
+      }
+    }
   }
 }
