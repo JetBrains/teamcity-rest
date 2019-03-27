bfc39f9ad5940343344dd8c015af5925543c6b47
Mikhail Khorkov <mikhail.khorkov@jetbrains.com>
2019-03-28 00:18:03 +0700

    TW-59755 REST: add build artifact size data
    /{buildLocator}/artifacts-sizes

---
+++ src/jetbrains/buildServer/server/rest/model/files/ArtifactsSizes.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2000-2019 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package jetbrains.buildServer.server.rest.model.files;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * Class for representing a size of whole artifacts of a build.
+ *
+ * @author Mikhail Khorkov
+ * @since 2019.1
+ */
+@SuppressWarnings("PublicField")
+@XmlRootElement(name = "artifacts-sizes")
+public class ArtifactsSizes {
+
+  @XmlElement(name = "artifacts-size")
+  public long artifactsSize;
+
+  @XmlElement(name = "hidden-artifacts-size")
+  public long hiddenArtifactsSize;
+
+  @XmlElement(name = "reported-artifacts-size")
+  public long reportedArtifactsSize;
+
+  @XmlElement(name = "reported-hidden-artifacts-size")
+  public long reportedHiddenArtifactsSize;
+
+  public ArtifactsSizes() {
+  }
+
+  public ArtifactsSizes(final long artifactsSize, final long hiddenArtifactsSize, final long reportedArtifactsSize, final long reportedHiddenArtifactsSize) {
+    this.artifactsSize = artifactsSize;
+    this.hiddenArtifactsSize = hiddenArtifactsSize;
+    this.reportedArtifactsSize = reportedArtifactsSize;
+    this.reportedHiddenArtifactsSize = reportedHiddenArtifactsSize;
+  }
+}
