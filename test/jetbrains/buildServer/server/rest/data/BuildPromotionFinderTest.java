9821f34f1b951efbdbe24a16a528dd85b61dc5d2
Kirill (KIR) Maximov <kir@maxkir.com>
2020-03-17 14:38:51 +0100

    Fix compilation rest trunk-protocol-2017.1

--- test/jetbrains/buildServer/server/rest/data/BuildPromotionFinderTest.java
+++ test/jetbrains/buildServer/server/rest/data/BuildPromotionFinderTest.java
@@ -1654,7 +1654,7 @@ public class BuildPromotionFinderTest extends BaseFinderTest<BuildPromotion> {
           }
         };
       }
-    }), false, "\n", "", "");
+    }), "\n", "", "");
   }
 
   public <E extends Throwable> void checkExceptionOnBuildSearch(final Class<E> exception, final String singleBuildLocator) {
