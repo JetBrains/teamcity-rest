f255e8c89555ad3bd00525b38168c288210e380f
Dmitrii Bogdanov <dmitrii.bogdanov@jetbrains.com>
2020-04-21 14:06:09 +0200

    Add storesProjectSettings field to Change

--- src/jetbrains/buildServer/server/rest/model/change/Change.java
+++ src/jetbrains/buildServer/server/rest/model/change/Change.java
@@ -23,6 +23,7 @@ import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
+import jetbrains.buildServer.Used;
 import jetbrains.buildServer.server.rest.ApiUrlBuilder;
 import jetbrains.buildServer.server.rest.data.ChangeFinder;
 import jetbrains.buildServer.server.rest.data.Locator;
@@ -37,24 +38,49 @@ import jetbrains.buildServer.server.rest.util.BeanContext;
 import jetbrains.buildServer.server.rest.util.ValueWithDefault;
 import jetbrains.buildServer.serverSide.WebLinks;
 import jetbrains.buildServer.users.SUser;
+import jetbrains.buildServer.vcs.RelationType;
 import jetbrains.buildServer.vcs.SVcsModification;
+import jetbrains.buildServer.vcs.impl.VcsModificationEx;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 /**
  * User: Yegor Yarko
  * Date: 12.04.2009
  */
 @XmlRootElement(name = "change")
-@XmlType(name = "change", propOrder = {"id", "version", "internalVersion", "username", "date", "registrationDate", "personal", "href", "webUrl",
-  "comment", "user", "fileChanges", "vcsRootInstance", "parentChanges", "parentRevisions", "attributes"})
+@XmlType(name = "change", propOrder = {
+  "id",
+  "version",
+  "internalVersion",
+  "username",
+  "date",
+  "registrationDate",
+  "personal",
+  "href",
+  "webUrl",
+  "comment",
+  "user",
+  "fileChanges",
+  "vcsRootInstance",
+  "parentChanges",
+  "parentRevisions",
+  "attributes",
+  "storesProjectSettings"
+})
 public class Change {
   protected SVcsModification myModification;
-  @NotNull private Fields myFields;
-  @NotNull private BeanContext myBeanContext;
   protected ApiUrlBuilder myApiUrlBuilder;
   protected WebLinks myWebLinks;
+  @NotNull private Fields myFields;
+  @NotNull private BeanContext myBeanContext;
+  /**
+   * These are used only when posting a link to the change
+   */
+  private String submittedLocator;
+  private Long submittedId;
+  private Boolean submittedPersonal;
 
+  @Used("javax.xml")
   public Change() {
   }
 
@@ -64,174 +90,6 @@ public class Change {
     myBeanContext = beanContext;
     myApiUrlBuilder = myBeanContext.getApiUrlBuilder();
     myWebLinks = myBeanContext.getSingletonService(WebLinks.class);
-  }
-
-  @XmlAttribute
-  public Long getId() {
-    return ValueWithDefault.decideDefault(myFields.isIncluded("id"), myModification.getId());
-  }
-
-  @XmlAttribute
-  public String getVersion() {
-    return ValueWithDefault.decideDefault(myFields.isIncluded("version"), myModification.getDisplayVersion());
-  }
-
-  @XmlAttribute
-  public String getInternalVersion() {
-    return ValueWithDefault.decideDefault(myFields.isIncluded("internalVersion", false, false), new ValueWithDefault.Value<String>() {
-      @Nullable
-      public String get() {
-        return myModification.getVersion();
-      }
-    });
-  }
-
-  @XmlAttribute
-  public Boolean getPersonal() {
-    return ValueWithDefault.decideDefault(myFields.isIncluded("personal"), myModification.isPersonal());
-  }
-
-  @XmlAttribute
-  public String getHref() {
-    return ValueWithDefault.decideDefault(myFields.isIncluded("href"), myApiUrlBuilder.getHref(myModification));
-  }
-
-  @XmlAttribute
-  public String getWebUrl() {
-    return ValueWithDefault.decideDefault(myFields.isIncluded("webUrl"), myWebLinks.getChangeUrl(myModification.getId(), myModification.isPersonal()));
-  }
-
-  @XmlAttribute
-  public String getUsername() {
-    return ValueWithDefault.decideDefault(myFields.isIncluded("username"), myModification.getUserName());
-  }
-
-  @XmlAttribute
-  public String getDate() {
-    final Date vcsDate = myModification.getVcsDate();
-    return ValueWithDefault.decideDefault(myFields.isIncluded("date"), Util.formatTime(vcsDate));
-  }
-
-  @XmlAttribute
-  public String getRegistrationDate() {
-    return ValueWithDefault.decideDefault(myFields.isIncluded("registrationDate", false, false), new ValueWithDefault.Value<String>() {
-      @Nullable
-      public String get() {
-        return Util.formatTime(myModification.getRegistrationDate());
-      }
-    });
-  }
-
-  @XmlElement
-  public String getComment() {
-    return ValueWithDefault.decideDefault(myFields.isIncluded("comment", false), myModification.getDescription());
-  }
-
-  @XmlElement(name = "user")
-  public User getUser() {
-    return ValueWithDefault.decideDefault(myFields.isIncluded("user", false), new ValueWithDefault.Value<User>() {
-      @Nullable
-      public User get() {
-        final Collection<SUser> users = myModification.getCommitters();
-        if (users.size() != 1){
-          return null;
-        }
-        return new User(users.iterator().next(), myFields.getNestedField("user"), myBeanContext);
-      }
-    });
-  }
-
-  @XmlElement(name = "files")
-  public FileChanges getFileChanges() {
-    return ValueWithDefault.decideDefault(myFields.isIncluded("files", false), new ValueWithDefault.Value<FileChanges>() {
-      @Nullable
-      public FileChanges get() {
-        return new FileChanges(myModification.getChanges(), myFields.getNestedField("files"));
-      }
-    });
-  }
-
-  @XmlElement(name = "vcsRootInstance")
-  public VcsRootInstance getVcsRootInstance() {
-    return myModification.isPersonal()
-           ? null
-           : ValueWithDefault.decideDefault(myFields.isIncluded("vcsRootInstance", false),
-                                            new VcsRootInstance(myModification.getVcsRoot(), myFields.getNestedField("vcsRootInstance"), myBeanContext));
-  }
-
-  @XmlElement(name = "parentChanges")
-  public Changes getParentChanges() {
-    return ValueWithDefault.decideDefault(myFields.isIncluded("parentChanges", false, false), new ValueWithDefault.Value<Changes>() {
-      @Nullable
-      public Changes get() {
-        return new Changes(new ArrayList<SVcsModification>(myModification.getParentModifications()), null, myFields.getNestedField("parentChanges"), myBeanContext);
-      }
-    });
-  }
-
-  @XmlElement(name = "parentRevisions")
-  public Items getParentRevisions() {
-    return ValueWithDefault.decideDefault(myFields.isIncluded("parentRevisions", false, false), new ValueWithDefault.Value<Items>() {
-      @Nullable
-      public Items get() {
-        return new Items(myModification.getParentRevisions());
-      }
-    });
-  }
-
-  /**
-   * experimental use only
-   */
-  @XmlElement(name = "attributes")
-  public Properties getAttributes() {
-    return ValueWithDefault.decideDefault(myFields.isIncluded("attributes", false, false),
-                                          () -> new Properties(myModification.getAttributes(), null, myFields.getNestedField("attributes"), myBeanContext));
-  }
-
-  /**
-   * These are used only when posting a link to the change
-   */
-  private String submittedLocator;
-  private Long submittedId;
-  private Boolean submittedPersonal;
-
-  public void setId(Long id) {
-    submittedId = id;
-  }
-
-  public void setPersonal(Boolean value) {
-    submittedPersonal = value;
-  }
-
-  @XmlAttribute
-  public String getLocator() {
-    return null;
-  }
-
-  public void setLocator(final String locator) {
-    submittedLocator = locator;
-  }
-
-  @NotNull
-  public SVcsModification getChangeFromPosted(final ChangeFinder changeFinder) {
-    String locatorText;
-    if (submittedId != null) {
-      if (submittedLocator != null){
-        throw new BadRequestException("Both 'locator' and 'id' attributes are specified. Only one should be present.");
-      }
-      final Locator locator = Locator.createEmptyLocator().setDimension(ChangeFinder.DIMENSION_ID, String.valueOf(submittedId));
-      if (submittedPersonal != null && submittedPersonal){
-        locator.setDimension(ChangeFinder.PERSONAL, "true");
-      }
-      locatorText = locator.getStringRepresentation();
-    } else{
-      if (submittedLocator == null){
-        throw new BadRequestException("No change specified. Either 'id' or 'locator' attribute should be present.");
-      }
-      locatorText = submittedLocator;
-    }
-
-    return changeFinder.getItem(locatorText);
   }
 
   public static String getFieldValue(final SVcsModification vcsModification, final String field) {
@@ -256,4 +114,164 @@ public class Change {
     }
     throw new NotFoundException("Field '" + field + "' is not supported. Supported fields are: 'id', 'version', 'username', 'date', 'personal', 'comment'.");
   }
+
+  private static boolean isVersionedSettings(@NotNull final SVcsModification change) {
+    final boolean[] isVersionedSettingsChange = new boolean[]{false};
+    ((VcsModificationEx)change).consumeRelations((s, relationType) -> {
+      if (relationType == RelationType.SETTINGS_AFFECT_BUILDS) {
+        isVersionedSettingsChange[0] = true;
+      }
+    });
+    return isVersionedSettingsChange[0];
+  }
+
+  @XmlAttribute
+  public Long getId() {
+    return ValueWithDefault.decideDefault(myFields.isIncluded("id"), myModification.getId());
+  }
+
+  public void setId(Long id) {
+    submittedId = id;
+  }
+
+  @XmlAttribute
+  public String getVersion() {
+    return ValueWithDefault.decideDefault(myFields.isIncluded("version"), myModification.getDisplayVersion());
+  }
+
+  @Used
+  @XmlAttribute
+  public String getInternalVersion() {
+    return ValueWithDefault.decideDefault(myFields.isIncluded("internalVersion", false, false), () -> myModification.getVersion());
+  }
+
+  @XmlAttribute
+  public Boolean getPersonal() {
+    return ValueWithDefault.decideDefault(myFields.isIncluded("personal"), myModification.isPersonal());
+  }
+
+  public void setPersonal(Boolean value) {
+    submittedPersonal = value;
+  }
+
+  @XmlAttribute
+  public String getHref() {
+    return ValueWithDefault.decideDefault(myFields.isIncluded("href"), myApiUrlBuilder.getHref(myModification));
+  }
+
+  @XmlAttribute
+  public String getWebUrl() {
+    return ValueWithDefault.decideDefault(myFields.isIncluded("webUrl"), myWebLinks.getChangeUrl(myModification.getId(), myModification.isPersonal()));
+  }
+
+  @XmlAttribute
+  public String getUsername() {
+    return ValueWithDefault.decideDefault(myFields.isIncluded("username"), myModification.getUserName());
+  }
+
+  @XmlAttribute
+  public String getDate() {
+    final Date vcsDate = myModification.getVcsDate();
+    return ValueWithDefault.decideDefault(myFields.isIncluded("date"), Util.formatTime(vcsDate));
+  }
+
+  @Used
+  @XmlAttribute
+  public String getRegistrationDate() {
+    return ValueWithDefault.decideDefault(myFields.isIncluded("registrationDate", false, false), () -> Util.formatTime(myModification.getRegistrationDate()));
+  }
+
+  @XmlElement
+  public String getComment() {
+    return ValueWithDefault.decideDefault(myFields.isIncluded("comment", false), myModification.getDescription());
+  }
+
+  @XmlElement(name = "user")
+  public User getUser() {
+    return ValueWithDefault.decideDefault(myFields.isIncluded("user", false), () -> {
+      final Collection<SUser> users = myModification.getCommitters();
+      if (users.size() != 1) {
+        return null;
+      }
+      return new User(users.iterator().next(), myFields.getNestedField("user"), myBeanContext);
+    });
+  }
+
+  @XmlElement(name = "files")
+  public FileChanges getFileChanges() {
+    return ValueWithDefault.decideDefault(myFields.isIncluded("files", false), () -> new FileChanges(myModification.getChanges(), myFields.getNestedField("files")));
+  }
+
+  @XmlElement(name = "vcsRootInstance")
+  public VcsRootInstance getVcsRootInstance() {
+    return myModification.isPersonal()
+           ? null
+           : ValueWithDefault.decideDefault(myFields.isIncluded("vcsRootInstance", false),
+                                            new VcsRootInstance(myModification.getVcsRoot(), myFields.getNestedField("vcsRootInstance"), myBeanContext));
+  }
+
+  @Used
+  @XmlElement(name = "parentChanges")
+  public Changes getParentChanges() {
+    return ValueWithDefault.decideDefault(myFields.isIncluded("parentChanges", false, false),
+                                          () -> new Changes(new ArrayList<>(myModification.getParentModifications()), null, myFields.getNestedField("parentChanges"),
+                                                            myBeanContext));
+  }
+
+  @Used
+  @XmlElement(name = "parentRevisions")
+  public Items getParentRevisions() {
+    return ValueWithDefault.decideDefault(myFields.isIncluded("parentRevisions", false, false), () -> new Items(myModification.getParentRevisions()));
+  }
+
+  /**
+   * experimental use only
+   */
+  @XmlElement(name = "attributes")
+  public Properties getAttributes() {
+    return ValueWithDefault.decideDefault(myFields.isIncluded("attributes", false, false),
+                                          () -> new Properties(myModification.getAttributes(), null, myFields.getNestedField("attributes"), myBeanContext));
+  }
+
+  /**
+   * experimental
+   */
+  @Used
+  @XmlAttribute(name = "storesProjectSettings")
+  public Boolean getStoresProjectSettings() {
+    return ValueWithDefault.decideDefault(myFields.isIncluded("storesProjectSettings", false, false),
+                                          () -> isVersionedSettings(myModification));
+  }
+
+  @XmlAttribute
+  public String getLocator() {
+    return null;
+  }
+
+  public void setLocator(final String locator) {
+    submittedLocator = locator;
+  }
+
+  @NotNull
+  public SVcsModification getChangeFromPosted(final ChangeFinder changeFinder) {
+    String locatorText;
+    if (submittedId != null) {
+      if (submittedLocator != null) {
+        throw new BadRequestException("Both 'locator' and 'id' attributes are specified. Only one should be present.");
+      }
+      final Locator locator = Locator.createEmptyLocator().setDimension(ChangeFinder.DIMENSION_ID, String.valueOf(submittedId));
+      if (submittedPersonal != null && submittedPersonal) {
+        locator.setDimension(ChangeFinder.PERSONAL, "true");
+      }
+      locatorText = locator.getStringRepresentation();
+    } else {
+      if (submittedLocator == null) {
+        throw new BadRequestException("No change specified. Either 'id' or 'locator' attribute should be present.");
+      }
+      locatorText = submittedLocator;
+    }
+
+    return changeFinder.getItem(locatorText);
+  }
+
 }
