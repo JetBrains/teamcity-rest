dc0d3f4eb4d99db33fd2d033b15c27e53972290f
Kirill (KIR) Maximov <kir@maxkir.com>
2020-08-16 19:19:07 +0200

    Fix REST test after adding supportTestRetry option

--- test/jetbrains/buildServer/server/rest/model/BuildTypeTest.java
+++ test/jetbrains/buildServer/server/rest/model/BuildTypeTest.java
@@ -527,7 +527,7 @@ public class BuildTypeTest extends BaseFinderTest<BuildTypeOrTemplate> {
     {
       BuildType buildType = new BuildType(new BuildTypeOrTemplate(bt10), new Fields("$long,settings($long,$locator(defaults:any))"), myBeanContext);
 
-      assertEquals(22, buildType.getSettings().properties.size());
+      assertEquals(23, buildType.getSettings().properties.size());
       parameterEquals(find(buildType.getSettings().properties, "buildNumberCounter"), "buildNumberCounter", "1", null);
       parameterEquals(find(buildType.getSettings().properties, "buildNumberPattern"), "buildNumberPattern", "aaa", null);
     }
