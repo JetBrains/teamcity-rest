f46d12dfef27c555dd64f9cfc2fef0ddcb93f874
Dmitrii Bogdanov <dmitrii.bogdanov@jetbrains.com>
2020-08-20 16:22:17 +0200

    Pure refactoring:
     - Use lambdas where possible

--- src/jetbrains/buildServer/server/rest/data/mutes/MuteFinder.java
+++ src/jetbrains/buildServer/server/rest/data/mutes/MuteFinder.java
@@ -16,6 +16,10 @@
 
 package jetbrains.buildServer.server.rest.data.mutes;
 
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import jetbrains.buildServer.BuildTypeDescriptor;
 import jetbrains.buildServer.ServiceLocator;
 import jetbrains.buildServer.server.rest.data.*;
 import jetbrains.buildServer.server.rest.data.problem.ProblemFinder;
@@ -39,10 +43,6 @@ import jetbrains.buildServer.users.SUser;
 import jetbrains.buildServer.users.User;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-
-import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static jetbrains.buildServer.server.rest.data.TypedFinderBuilder.Dimension;
 
@@ -90,6 +90,100 @@ public class MuteFinder extends DelegatingFinder<MuteInfo> {
     setDelegate(new MuteFinderBuilder().build());
   }
 
+  @NotNull
+  public static String getLocator(@NotNull final STest test) {
+    return Locator.getStringLocator(TEST.name, TestFinder.getTestLocator(test));
+  }
+
+  @NotNull
+  public static String getLocator(@NotNull final ProblemWrapper problem) {
+    return Locator.getStringLocator(PROBLEM.name, ProblemFinder.getLocator(problem));
+  }
+
+  @NotNull
+  public static String getLocator(final MuteInfo item) {
+    return Locator.getStringLocator(ID.name, String.valueOf(item.getId()));
+  }
+
+  private boolean canView(@NotNull final MuteInfo item) {
+    try {
+      MuteScope scope = item.getScope();
+      String projectId = scope.getProjectId();
+      if (projectId != null) {
+        myPermissionChecker.checkProjectPermission(Permission.VIEW_PROJECT, scope.getProjectId());
+      }
+
+      Collection<String> buildTypeIds = scope.getBuildTypeIds();
+      if (buildTypeIds != null) {
+        ProjectManager projectManager = myServiceLocator.getSingletonService(ProjectManager.class);
+        buildTypeIds.stream().map(projectManager::findBuildTypeById).filter(Objects::nonNull).map(BuildTypeDescriptor::getProjectId).collect(Collectors.toSet())
+                    .forEach(pId -> myPermissionChecker.checkProjectPermission(Permission.VIEW_PROJECT, pId)); //todo: should actually filter out data on MuteInfo
+      }
+      Long buildId = scope.getBuildId();
+      if (buildId != null) {
+        //actually, this should never happen todo: check this
+        BuildPromotion buildPromotion = findPromotionByBuildId(buildId);
+        if (buildPromotion != null) {
+          myPermissionChecker.checkPermission(Permission.VIEW_PROJECT, buildPromotion);
+        }
+      }
+    } catch (AuthorizationFailedException | AccessDeniedException e) {
+      return false;
+    }
+    return true;
+  }
+
+  /*
+  @NotNull
+  public static String getLocator(@NotNull final SBuildType buildType) {
+    return Locator.createEmptyLocator().setDimension(BUILD_TYPE, BuildTypeFinder.getLocator(buildType)).getStringRepresentation();
+  }
+
+  @NotNull
+  public static String getLocatorForProblem(final int problemId, @NotNull BuildProject project) {
+    return InvestigationFinder.getLocatorForProblem(problemId, project);
+  }
+
+  @NotNull
+  public static String getLocatorForTest(final long testNameId, @NotNull BuildProject project) {
+    return InvestigationFinder.getLocatorForTest(testNameId, project);
+  }
+  */
+
+  @NotNull
+  private Stream<MuteInfo> getMuteInfosForProject(@NotNull final SProject project) {
+    return Stream.concat(getProblemsMutes(project), getTestsMutes(project)).sorted(Comparator.comparing(MuteInfo::getId));
+  }
+
+  @Nullable
+  private BuildPromotion findPromotionByBuildId(@NotNull final Long buildId) {
+    SBuild buildInstanceById = myServiceLocator.getSingletonService(BuildsManager.class).findBuildInstanceById(buildId);
+    return buildInstanceById == null ? null : buildInstanceById.getBuildPromotion();
+  }
+
+  @NotNull
+  private MuteInfo findMuteById(@NotNull final Integer id) {
+    Optional<MuteInfo> result = getMuteInfosForProject(myProjectFinder.getRootProject()).filter(muteInfo -> id.equals(muteInfo.getId())).findAny(); //todo: not optimal at all
+    if (result.isPresent()) return result.get();
+    throw new NotFoundException("No mute with id '" + id + "' found");
+
+    /* this returns the original mutes state, so does not work (TW-53393)
+    String projectId = myProjectFinder.getRootProject().getProjectId();
+
+    Collection<Long> mutedTestNameIds = myLowLevelMutingService.retrieveMuteTests(id);
+    Optional<MuteInfo> result = mutedTestNameIds.stream().flatMap(testNameId -> getMutes(myProblemMutingService.getTestCurrentMuteInfo(projectId, testNameId)))
+                                             .filter(muteInfo -> id.equals(muteInfo.getId())).findAny();
+    if (result.isPresent()) return result.get();
+
+    Collection<Integer> mutedProblemIds = myLowLevelMutingService.retrieveMuteProblems(id);
+    result = mutedProblemIds.stream().flatMap(problemId -> getMutes(myProblemMutingService.getBuildProblemCurrentMuteInfo(projectId, problemId)))
+                                            .filter(muteInfo -> id.equals(muteInfo.getId())).findAny();
+    if (result.isPresent()) return result.get();
+
+    throw new NotFoundException("No mute with id '" + id + "' found");
+    */
+  }
+
   private class MuteFinderBuilder extends TypedFinderBuilder<MuteInfo> {
 
     MuteFinderBuilder() {
@@ -124,27 +218,29 @@ public class MuteFinder extends DelegatingFinder<MuteInfo> {
                                                     //      myProblemMutingService.getBuildProblemCurrentMuteInfo(rootProjectId, problem.getId().intValue());  //this does not return all the muting data, only for the problem passed
                                                     //    if (currentMuteInfo == null) return Stream.empty();
                                                     //    getActualCurrentMuteProblems(problem.getId().intValue(), currentMuteInfo, result);
-                                                    //    return result.values().stream();
-                                                    //  }).collect(Collectors.toList()));
+      //    return result.values().stream();
+      //  }).collect(Collectors.toList()));
 
       dimensionProjects(AFFECTED_PROJECT, myServiceLocator).description("project affected by the mutes")
                                                            .filter((projects, item) -> {
                                                              final SProject assignmentProject = item.getProject();
                                                              return (assignmentProject != null && ProjectFinder.isSameOrParent(projects, assignmentProject));
-                                                           }).toItems(dimension -> dimension.stream().flatMap(p -> getMuteInfosForProject(p)).collect(Collectors.toList()));
+                                                           })
+                                                           .toItems(dimension -> dimension.stream().flatMap(MuteFinder.this::getMuteInfosForProject).collect(Collectors.toList()));
 
-      dimensionProjects(PROJECT, myServiceLocator).description("project in which mute is assigned").valueForDefaultFilter(muteInfo -> Collections.singleton(muteInfo.getProject())); //todo: add toItems?
+      dimensionProjects(PROJECT, myServiceLocator).description("project in which mute is assigned")
+                                                  .valueForDefaultFilter(muteInfo -> Collections.singleton(muteInfo.getProject())); //todo: add toItems?
 
       dimensionTimeCondition(CREATION_DATE, myTimeCondition).description("mute creation time")
-                                                   .filter((timeCondition, item) -> timeCondition.matches(item.getMutingTime()));
+                                                            .filter((timeCondition, item) -> timeCondition.matches(item.getMutingTime()));
       dimensionTimeCondition(UNMUTE_DATE, myTimeCondition).description("automatic unmute time")
-                                                   .filter((timeCondition, item) -> {
-                                                     Date unmuteTime = item.getAutoUnmuteOptions().getUnmuteByTime();
-                                                     return unmuteTime != null && timeCondition.matches(unmuteTime);
-                                                   });
+                                                          .filter((timeCondition, item) -> {
+                                                            Date unmuteTime = item.getAutoUnmuteOptions().getUnmuteByTime();
+                                                            return unmuteTime != null && timeCondition.matches(unmuteTime);
+                                                          });
       dimensionUsers(REPORTER, myServiceLocator).description("muting user")
-                                                .filter((users, item) -> users.stream().map(u -> u.getId()).collect(Collectors.toSet()).contains(item.getMutingUserId()));
-      dimensionFixedText(TYPE, ProblemTarget.getKnownTypesForMute()).description("what is muted").valueForDefaultFilter(muteInfo -> ProblemTarget.getType(muteInfo))
+                                                .filter((users, item) -> users.stream().map(User::getId).collect(Collectors.toSet()).contains(item.getMutingUserId()));
+      dimensionFixedText(TYPE, ProblemTarget.getKnownTypesForMute()).description("what is muted").valueForDefaultFilter(ProblemTarget::getType)
                                                                     .toItems(dimension -> {
                                                                       switch (dimension) {
                                                                         case ProblemTarget.TEST_TYPE:
@@ -173,103 +269,9 @@ public class MuteFinder extends DelegatingFinder<MuteInfo> {
 
       defaults(DimensionCondition.ALWAYS, new NameValuePairs().add(PagerData.COUNT, String.valueOf(Constants.getDefaultPageItemsCount())));
 
-      locatorProvider(muteInfo -> getLocator(muteInfo));
+      locatorProvider(MuteFinder::getLocator);
 //      containerSetProvider(() -> new TreeSet<MuteInfo>(Comparator.comparing(muteInfo -> muteInfo.getId()))); //todo: implement sorting here
     }
-  }
-
-  private boolean canView(@NotNull final MuteInfo item) {
-    try {
-      MuteScope scope = item.getScope();
-      String projectId = scope.getProjectId();
-      if (projectId != null) {
-        myPermissionChecker.checkProjectPermission(Permission.VIEW_PROJECT, scope.getProjectId());
-      }
-
-      Collection<String> buildTypeIds = scope.getBuildTypeIds();
-      if (buildTypeIds != null) {
-        ProjectManager projectManager = myServiceLocator.getSingletonService(ProjectManager.class);
-        buildTypeIds.stream().map(id -> projectManager.findBuildTypeById(id)).filter(Objects::nonNull).map(buildType -> buildType.getProjectId()).collect(Collectors.toSet()).forEach(pId -> myPermissionChecker.checkProjectPermission(Permission.VIEW_PROJECT, pId)); //todo: should actually filter out data on MuteInfo
-      }
-      Long buildId = scope.getBuildId();
-      if (buildId != null) {
-        //actually, this should never happen todo: check this
-        BuildPromotion buildPromotion = findPromotionByBuildId(buildId);
-        if (buildPromotion != null) {
-          myPermissionChecker.checkPermission(Permission.VIEW_PROJECT, buildPromotion);
-        }
-      }
-    } catch (AuthorizationFailedException | AccessDeniedException e) {
-      return false;
-    }
-    return true;
-  }
-
-  @Nullable
-  private BuildPromotion findPromotionByBuildId(@NotNull final Long buildId) {
-    SBuild buildInstanceById = myServiceLocator.getSingletonService(BuildsManager.class).findBuildInstanceById(buildId);
-    return buildInstanceById  == null ? null : buildInstanceById.getBuildPromotion();
-  }
-
-  @NotNull
-  private MuteInfo findMuteById(@NotNull final Integer id) {
-    Optional<MuteInfo> result = getMuteInfosForProject(myProjectFinder.getRootProject()).filter(muteInfo -> id.equals(muteInfo.getId())).findAny(); //todo: not optimal at all
-    if (result.isPresent()) return result.get();
-    throw new NotFoundException("No mute with id '" + id + "' found");
-
-    /* this returns the original mutes state, so does not work (TW-53393)
-    String projectId = myProjectFinder.getRootProject().getProjectId();
-
-    Collection<Long> mutedTestNameIds = myLowLevelMutingService.retrieveMuteTests(id);
-    Optional<MuteInfo> result = mutedTestNameIds.stream().flatMap(testNameId -> getMutes(myProblemMutingService.getTestCurrentMuteInfo(projectId, testNameId)))
-                                             .filter(muteInfo -> id.equals(muteInfo.getId())).findAny();
-    if (result.isPresent()) return result.get();
-
-    Collection<Integer> mutedProblemIds = myLowLevelMutingService.retrieveMuteProblems(id);
-    result = mutedProblemIds.stream().flatMap(problemId -> getMutes(myProblemMutingService.getBuildProblemCurrentMuteInfo(projectId, problemId)))
-                                            .filter(muteInfo -> id.equals(muteInfo.getId())).findAny();
-    if (result.isPresent()) return result.get();
-
-    throw new NotFoundException("No mute with id '" + id + "' found");
-    */
-  }
-
-  /*
-  @NotNull
-  public static String getLocator(@NotNull final SBuildType buildType) {
-    return Locator.createEmptyLocator().setDimension(BUILD_TYPE, BuildTypeFinder.getLocator(buildType)).getStringRepresentation();
-  }
-
-  @NotNull
-  public static String getLocatorForProblem(final int problemId, @NotNull BuildProject project) {
-    return InvestigationFinder.getLocatorForProblem(problemId, project);
-  }
-
-  @NotNull
-  public static String getLocatorForTest(final long testNameId, @NotNull BuildProject project) {
-    return InvestigationFinder.getLocatorForTest(testNameId, project);
-  }
-  */
-
-  @NotNull
-  public static String getLocator(@NotNull final STest test) {
-    return Locator.getStringLocator(TEST.name, TestFinder.getTestLocator(test));
-  }
-
-  @NotNull
-  public static String getLocator(@NotNull final ProblemWrapper problem) {
-    return Locator.getStringLocator(PROBLEM.name, ProblemFinder.getLocator(problem));
-  }
-
-  @NotNull
-  public static String getLocator(final MuteInfo item) {
-    return Locator.getStringLocator(ID.name, String.valueOf(item.getId()));
-  }
-
-
-  @NotNull
-  private Stream<MuteInfo> getMuteInfosForProject(@NotNull final SProject project) {
-    return Stream.concat(getProblemsMutes(project), getTestsMutes(project)).sorted(Comparator.comparing(muteInfo -> muteInfo.getId()));
   }
 
   @NotNull
@@ -359,8 +361,8 @@ public class MuteFinder extends DelegatingFinder<MuteInfo> {
       return this;
     }
 
-    public MuteInfoWrapper addBuildType(@NotNull String buidlTypeInternalId) {
-      myBuildTypeIds.add(buidlTypeInternalId);
+    public MuteInfoWrapper addBuildType(@NotNull String buildTypeInternalId) {
+      myBuildTypeIds.add(buildTypeInternalId);
       return this;
     }
 
@@ -447,7 +449,7 @@ public class MuteFinder extends DelegatingFinder<MuteInfo> {
     public Collection<STest> getTests() {
       // see MuteInfoRecord.getTests()
       Collection<Long> ids = getTestNameIds();
-      List<STest> tests = new ArrayList<STest>(ids.size());
+      List<STest> tests = new ArrayList<>(ids.size());
       for (Long id: ids) {
         final STest test = myLowLevelMutingService.getTestManager().findTest(id, getProjectId());
         if (test != null)
