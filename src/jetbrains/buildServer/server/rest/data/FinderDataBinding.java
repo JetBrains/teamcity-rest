30d4de500b82f8d2c709702e7bf9e75a7048ff46
andrey.kokorev <andrey.kokorev@jetbrains.com>
2021-07-19 15:34:56 +0300

    REST: Added wrapping item holder, wrapping items on the fly for processing + Unwrapping filter, unwrapping items on the fly.

--- src/jetbrains/buildServer/server/rest/data/FinderDataBinding.java
+++ src/jetbrains/buildServer/server/rest/data/FinderDataBinding.java
@@ -19,6 +19,7 @@ package jetbrains.buildServer.server.rest.data;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+import java.util.function.Function;
 import java.util.stream.Stream;
 import jetbrains.buildServer.server.rest.errors.NotFoundException;
 import jetbrains.buildServer.util.ItemProcessor;
@@ -95,6 +96,31 @@ public interface FinderDataBinding<ITEM> {
   @NotNull
   static <P> ItemHolder<P> getItemHolder(@NotNull Iterable<? extends P> items) {
     return new CollectionItemHolder<P>(items);
+  }
+
+  static class WrappingItemHolder<UNWRAPPED, WRAPPED> implements ItemHolder<WRAPPED> {
+    private final Function<UNWRAPPED, WRAPPED> myWrapper;
+    private final ItemHolder<? extends UNWRAPPED> myDelegate;
+
+    public WrappingItemHolder(@NotNull final ItemHolder<? extends UNWRAPPED> delegate, @NotNull Function<UNWRAPPED, WRAPPED> wrapper) {
+      myWrapper = wrapper;
+      myDelegate = delegate;
+    }
+
+    public WrappingItemHolder(@NotNull final Iterable<? extends UNWRAPPED> items, @NotNull Function<UNWRAPPED, WRAPPED> wrapper) {
+      myWrapper = wrapper;
+      myDelegate = new CollectionItemHolder<>(items);
+    }
+
+    public WrappingItemHolder(@NotNull final Stream<? extends UNWRAPPED> items, @NotNull Function<UNWRAPPED, WRAPPED> wrapper) {
+      myWrapper = wrapper;
+      myDelegate = getItemHolder(items);
+    }
+
+    @Override
+    public void process(@NotNull ItemProcessor<WRAPPED> processor) {
+      myDelegate.process(item -> processor.processItem(myWrapper.apply(item)));
+    }
   }
 
   static class CollectionItemHolder<P> implements ItemHolder<P> {
---
+++ src/jetbrains/buildServer/server/rest/data/UnwrappingFilter.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2000-2021 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package jetbrains.buildServer.server.rest.data;
+
+import jetbrains.buildServer.vcs.Function;
+import org.jetbrains.annotations.NotNull;
+
+public class UnwrappingFilter<NAKED, WRAPPED> implements ItemFilter<WRAPPED> {
+  private final ItemFilter<NAKED> myNakedFilter;
+  private final Function<WRAPPED, NAKED> myUnwrapper;
+
+  public UnwrappingFilter(@NotNull ItemFilter<NAKED> nakedFilter, @NotNull Function<WRAPPED, NAKED> unwrapper) {
+    myNakedFilter = nakedFilter;
+    myUnwrapper = unwrapper;
+  }
+
+  @Override
+  public boolean isIncluded(@NotNull WRAPPED item) {
+    return myNakedFilter.isIncluded(myUnwrapper.apply(item));
+  }
+
+  @Override
+  public boolean shouldStop(@NotNull WRAPPED item) {
+    return myNakedFilter.shouldStop(myUnwrapper.apply(item));
+  }
+}
