c1821fe0e2f0107213ee30048b7bb7e4a55c97e3
Vladimir Shefer <vladimir.shefer@jetbrains.com>
2022-11-16 18:36:05 +0400

    REST: Minor refactoring in TypedFinderBuilder

--- src/jetbrains/buildServer/server/rest/data/TypedFinderBuilder.java
+++ src/jetbrains/buildServer/server/rest/data/TypedFinderBuilder.java
@@ -298,7 +298,7 @@ public class TypedFinderBuilder<ITEM> {
   /**
    * Defines a dimension, which supplies intermediate items for purpouses of filtering or further extracting final ITEMs. <br/>
    * <br/>
-   * Use with caution: should be able to find items in Set!
+   * @implNote Use with caution: should be able to find items in Set!
    */
   public <FINDER_TYPE> TypedFinderDimensionWithDefaultChecker<ITEM, List<FINDER_TYPE>, Set<FINDER_TYPE>> dimensionWithFinder(
     @NotNull final Dimension<List<FINDER_TYPE>> dimension,
