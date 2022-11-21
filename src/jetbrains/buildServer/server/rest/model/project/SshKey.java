ff2e882b42dadceb8c6a009040edf1300e610c00
Vladimir Shefer <vladimir.shefer@jetbrains.com>
2022-11-22 01:18:47 +0400

    REST: TW-42311 SshKey dtos

---
+++ src/jetbrains/buildServer/server/rest/model/project/SshKey.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2000-2022 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package jetbrains.buildServer.server.rest.model.project;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+import jetbrains.buildServer.server.rest.swagger.annotations.ModelDescription;
+
+/**
+ * @author Vladimir Shefer
+ * @date 21.11.2022
+ */
+@XmlRootElement(name = "sshKey")
+@XmlType(name = "sshKey")
+@ModelDescription(
+  value = "Represents ssh key.",
+  externalArticleLink = "https://www.jetbrains.com/help/teamcity/ssh-keys-management.html#Uploading+SSH+Key+to+TeamCity+Server",
+  externalArticleName = "Uploading SSH Key to TeamCity Server"
+)
+public class SshKey {
+
+  private String myName;
+
+  private Boolean myIsEncrypted;
+
+  @XmlAttribute
+  public String getName() {
+    return myName;
+  }
+
+  public void setName(String name) {
+    this.myName = name;
+  }
+
+  @XmlAttribute
+  public Boolean getEncrypted() {
+    return myIsEncrypted;
+  }
+
+  public void setEncrypted(Boolean encrypted) {
+    myIsEncrypted = encrypted;
+  }
+
+}
