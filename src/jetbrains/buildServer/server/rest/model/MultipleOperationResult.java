0640671d7c8fc5614be1c761ffc9ed55fce5efa1
Vladimir Shefer <vladimir.shefer@jetbrains.com>
2023-01-04 15:30:51 +0400

    REST: Minor: MultipleOperationResult optimize imports

--- src/jetbrains/buildServer/server/rest/model/MultipleOperationResult.java
+++ src/jetbrains/buildServer/server/rest/model/MultipleOperationResult.java
@@ -25,11 +25,9 @@ import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
-import io.swagger.annotations.ExtensionProperty;
 import jetbrains.buildServer.server.rest.data.Finder;
 import jetbrains.buildServer.server.rest.errors.BadRequestException;
 import jetbrains.buildServer.server.rest.swagger.annotations.ModelBaseType;
-import jetbrains.buildServer.server.rest.swagger.constants.ExtensionType;
 import jetbrains.buildServer.server.rest.swagger.constants.ObjectType;
 import jetbrains.buildServer.server.rest.util.BeanContext;
 import jetbrains.buildServer.server.rest.util.ValueWithDefault;
