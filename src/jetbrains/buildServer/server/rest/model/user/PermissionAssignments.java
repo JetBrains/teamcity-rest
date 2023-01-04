8c757094001b742909fbf0a8af9a6105cdee85e3
Vladimir Shefer <vladimir.shefer@jetbrains.com>
2023-01-04 15:30:32 +0400

    REST: Minor: PermissionAssignments optimize imports

--- src/jetbrains/buildServer/server/rest/model/user/PermissionAssignments.java
+++ src/jetbrains/buildServer/server/rest/model/user/PermissionAssignments.java
@@ -23,11 +23,9 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
-import io.swagger.annotations.ExtensionProperty;
 import jetbrains.buildServer.server.rest.data.PermissionAssignmentFinder;
 import jetbrains.buildServer.server.rest.model.Fields;
 import jetbrains.buildServer.server.rest.swagger.annotations.ModelBaseType;
-import jetbrains.buildServer.server.rest.swagger.constants.ExtensionType;
 import jetbrains.buildServer.server.rest.swagger.constants.ObjectType;
 import jetbrains.buildServer.server.rest.util.BeanContext;
 import jetbrains.buildServer.server.rest.util.ValueWithDefault;
