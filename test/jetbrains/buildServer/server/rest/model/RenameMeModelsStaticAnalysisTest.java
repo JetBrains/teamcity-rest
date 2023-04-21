c8d48d227c9ddbdd2813129c8f6307e34a9cb21e
Vladimir Shefer <vladimir.shefer@jetbrains.com>
2023-04-21 14:41:40 +0000

    REST: More model tests
    REST: Debug commit
    
    REST: Debug commit
    
    REST: Test that models' xml fields are distinct
    
    Merge-request: TC-MR-4871
    Merged-by: Vladimir Shefer <Vladimir.Shefer@jetbrains.com>

--- test/jetbrains/buildServer/server/rest/model/RenameMeModelsStaticAnalysisTest.java
+++ test/jetbrains/buildServer/server/rest/model/RenameMeModelsStaticAnalysisTest.java
@@ -17,7 +17,9 @@
 package jetbrains.buildServer.server.rest.model;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.lang.reflect.Member;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -30,8 +32,10 @@ import javax.xml.bind.annotation.XmlType;
 import jetbrains.buildServer.server.rest.swagger.annotations.ModelBaseType;
 import jetbrains.buildServer.server.rest.swagger.annotations.ModelDescription;
 import jetbrains.buildServer.server.rest.testUtil.ClasspathScanner;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static org.testng.AssertJUnit.fail;
@@ -40,7 +44,7 @@ import static org.testng.AssertJUnit.fail;
  * This class is a replacement for static analysis tool.
  * Tests in this class go through all model classes via reflection and verify if the class sctructure is valid.
  */
-public class ModelsStaticAnalysisTest {
+public class RenameMeModelsStaticAnalysisTest {
 
   private List<Class<?>> modelTypes;
 
@@ -54,12 +58,36 @@ public class ModelsStaticAnalysisTest {
   }
 
   @Test
-  public void testPropertiesAreListedInXmlTypeAnnotation() {
-    for (Class<?> type : modelTypes) {
-      if (type.isAnnotationPresent(XmlType.class)) {
-        validateXmlTypeAnnotation(type, getPropertyNamesAnnotated(type).stream().map(ModelProperty::getJavaName).collect(Collectors.toList()));
+  public void testDummyTest() {
+  }
+
+  @DataProvider(name= "modelTypes")
+  public Object[][] getModelTypes() {
+    return modelTypes.stream().map(it -> new Object[]{it}).collect(Collectors.toList()).toArray(new Object[0][0]);
+  }
+
+  @Test(dataProvider = "modelTypes")
+  public void testXmlPropertiesAreDistinct(Class<?> type) {
+      List<ModelProperty> propertyNamesAnnotated = getPropertyNamesAnnotated(type);
+      List<String> duplicatePropertyReports = propertyNamesAnnotated
+        .stream()
+        .collect(Collectors.groupingBy(it -> it.getFinalXmlName()))
+        .entrySet()
+        .stream()
+        .filter(name2property -> name2property.getValue().size() > 1)
+        .map(name2property -> name2property.getKey() + " met " + name2property.getValue().size() + " times")
+        .collect(Collectors.toList());
+      if (!duplicatePropertyReports.isEmpty()) {
+        fail(type.getName() + " has duplicating xml properties: " + duplicatePropertyReports);
       }
-    }
+  }
+
+  @Test(dataProvider = "modelTypes")
+  public void testPropertiesAreListedInXmlTypeAnnotation(Class<?> type) {
+      List<ModelProperty> propertyNamesAnnotated = getPropertyNamesAnnotated(type);
+      if (type.isAnnotationPresent(XmlType.class)) {
+        validateXmlTypeAnnotation(type, propertyNamesAnnotated.stream().map(ModelProperty::getJavaName).collect(Collectors.toList()));
+      }
   }
 
   private List<ModelProperty> getPropertyNamesAnnotated(Class<?> type) {
@@ -75,15 +103,20 @@ public class ModelsStaticAnalysisTest {
   }
 
   private String getPropertyNameForGetter(Member method) {
-    if (method.getName().startsWith("get")) {
-      String propertyNameCapitalized = method.getName().substring("get".length());
-      return propertyNameCapitalized.substring(0, 1).toLowerCase() + propertyNameCapitalized.substring(1);
+    if (method instanceof Field) {
+      return method.getName();
     }
-    if (method.getName().startsWith("is")) {
-      String propertyNameCapitalized = method.getName().substring("is".length());
-      return propertyNameCapitalized.substring(0, 1).toLowerCase() + propertyNameCapitalized.substring(1);
+    if (method instanceof Method) {
+      if (method.getName().startsWith("get")) {
+        String propertyNameCapitalized = method.getName().substring("get".length());
+        return propertyNameCapitalized.substring(0, 1).toLowerCase() + propertyNameCapitalized.substring(1);
+      }
+      if (method.getName().startsWith("is")) {
+        String propertyNameCapitalized = method.getName().substring("is".length());
+        return propertyNameCapitalized.substring(0, 1).toLowerCase() + propertyNameCapitalized.substring(1);
+      }
     }
-    return method.getName();
+    throw new IllegalArgumentException("???");
   }
 
   private static void validateXmlTypeAnnotation(Class<?> myType, List<String> propertyNames) {
@@ -122,8 +155,14 @@ public class ModelsStaticAnalysisTest {
     }
 
     @Nullable
-    public String getXmlName() {
+    public String getDeclaredXmlName() {
       return xmlName;
+    }
+
+    @NotNull
+    public String getFinalXmlName() {
+      String xmlName = getDeclaredXmlName();
+      return (xmlName != null && !xmlName.equals("##default")) ? xmlName : getJavaName();
     }
   }
 
