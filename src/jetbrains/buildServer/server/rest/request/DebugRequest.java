5d1761a9cc76b044d424572f2338bb36ff782cb1
Pavel Sher <pavel.sher@jetbrains.com>
2023-06-15 10:47:43 +0000

    another attempt to speedup build queue optimizer; thread dumps show that we spend significant time fetching builds one by one, so it might be beneficial to switch to batch approach
    Merge-request: TC-MR-5643
    Merged-by: Pavel Sher <Pavel.Sher@jetbrains.com>

--- src/jetbrains/buildServer/server/rest/request/DebugRequest.java
+++ src/jetbrains/buildServer/server/rest/request/DebugRequest.java
@@ -552,7 +552,11 @@ public class DebugRequest {
     StringBuffer log = new StringBuffer();
     log.append("Optimization log for ").append(LogUtil.describe(build)).append('\n');
 
-    GraphOptimizer optimizer = new GraphOptimizer((BuildPromotionEx)build, myServiceLocator.getSingletonService(BuildPromotionReplacementLog.class), btId -> Collections.emptyList(), Collections.emptySet());
+    GraphOptimizer optimizer = new GraphOptimizer((BuildPromotionEx)build,
+                                                  myServiceLocator.getSingletonService(BuildPromotionReplacementLog.class),
+                                                  ids -> myServiceLocator.getSingletonService(BuildsManager.class).findBuildInstances(ids),
+                                                  btId -> Collections.emptyList(),
+                                                  Collections.emptySet());
     optimizer.dryRunOptimization(new OptimizationListener() {
       @Override
       public void equivalentBuildPromotionIgnored(@NotNull final BuildPromotionEx promotion, @NotNull final String reason) {
